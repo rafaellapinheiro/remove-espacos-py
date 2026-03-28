db1f324f427314bbbac3cfb8d76c42ba9fd13904
rafaellapinheiro <rafaellapinheiro.ti@gmail.com>
2026-03-28 09:35:19 -0300

    Projeto em Python para remoção de espaços

---
+++ remove_espacos.py
@@ -0,0 +1,12 @@
+texto = input("Digite uma frase: ")
+lista = list(texto)
+
+while lista and lista [0] == " ":
+    lista.pop(0)
+
+while lista and lista[-1] == " ":
+    lista.pop()
+
+resultado = "".join(lista)
+
+print("Resultado:", resultado)
